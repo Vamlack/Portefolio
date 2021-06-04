47cb0fd58c9eddc26f974cec976999d81e34e001
Vamlack <thomas.miras.garcia@efrei.net>
2021-06-04 22:12:07 +0200

    boite contact

--- Tableau-dynamique.js
+++ Tableau-dynamique.js
@@ -3,11 +3,13 @@ var ligne = new int;
 var colonne = new colonne;
 ligne = 0;
 colonne = 0;
+
+var nameValue = new string;
 if(window.fetch){
   while(lignes <=5){
 
     while colonne(<=5){
-      demander
+      nameValue = document.getElementById(uniqueID").value;
     }
     ligne =+ 1;
     colonne = 0;
